52e09f51e5b84a20f5704a7cab9fcc6b48cacd20
DESKTOP-104DH1D\Administrator <cyj8502@naver.com>
2023-05-21 01:35:49 +0900

    programmers 0521

--- programmers/src/프로그래머스Lv2/예상대진표.java
+++ programmers/src/프로그래머스Lv2/예상대진표.java
@@ -3,20 +3,20 @@ package 프로그래머스Lv2;
 public class 예상대진표 {
 
 	public static void main(String[] args) {
-		int n = 8;
+		//int n = 8;
 		int a = 4;
 		int b = 7;
 		int answer = 0;
-		
+
 		while (true) {
-			a= a/2+a%2;
-			b = b/2+ b%2;
+			a = a / 2 + a % 2;
+			b = b / 2 + b % 2;
 			answer++;
-			if(a==b) {
+			if (a == b) {
 				break;
 			}
 		}
-		
+
 		System.out.println(answer);
 
 	}
