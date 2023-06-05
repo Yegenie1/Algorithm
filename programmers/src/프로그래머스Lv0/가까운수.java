2edd4e0b1912da677406fc44f326592eb23ffb71
DESKTOP-104DH1D\Administrator <cyj8502@naver.com>
2023-06-05 20:13:00 +0900

    가까운수 런타임에러 해결

--- programmers/src/프로그래머스Lv0/가까운수.java
+++ programmers/src/프로그래머스Lv0/가까운수.java
@@ -3,23 +3,26 @@ package 프로그래머스Lv0;
 import java.util.Arrays;
 
 public class 가까운수 {
-public static void main(String[] args) {
-	int[] array = {3, 10, 28};
-	int n = 20;
-	int answer = 0;
-	Arrays.sort(array);
-	for (int i = 0; i < array.length; i++) {
-		if (array[i] >= n) {
-			int left = n - array[i-1];
-			int right = array[i]-n;
-			if (left<=right) {
-				answer = array[i-1];
-			}else {
-				answer = array[i];
-			}
-		}
-		answer = array[array.length-1];
+	public static void main(String[] args) {
+		int[] array = { 3, 10,20, 28 };
+		int n = 20;
+		int answer = 0;
+		Arrays.sort(array);
+	    if (array[0] >= n) {
+	        answer = array[0];
+	    }else {
+		    for (int i = 0; i < array.length; i++) {
+		        if (array[i] >= n) {
+		            int left = n - array[(i - 1)];
+		            int right = array[i] - n;
+		            answer = left <= right ? array[(i - 1)] : array[i];
+		            break;
+		        }else if(array[array.length-1] < n) {
+		        	answer = array[array.length - 1];
+		        }
+		    }
+	    }
+	    
+		System.out.println(answer);
 	}
-	System.out.println(answer);
-}
 }
