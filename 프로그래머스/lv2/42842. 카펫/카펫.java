a0f899e6bc219e3ddffacddc04cdfc88fef4d961
yejineeeeeeeee <124111019+Yegenie1@users.noreply.github.com>
2023-06-29 14:50:18 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,24 +1,9 @@
+import java.util.*;
 class Solution {
-    public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-		int sum = brown + yellow;
-		
-		for (int i = 3; i < sum; i++) {
-			int j = sum / i;
-			
-			if (sum % i == 0 && j >= 3) {
-				int col = Math.max(i, j);
-                int row = Math.min(i, j);
-				int yellowArea = (col - 2) * (row - 2);
-				
-				if (yellowArea == yellow) {
-					answer[0] = col;
-					answer[1] = row;
-					return answer;
-				}
-			}
-		}
-        
+    public int[] solution(int brown, int red) {
+        int a = (brown+4)/2;
+        int b = red+2*a-4;
+        int[] answer = {(int)(a+Math.sqrt(a*a-4*b))/2,(int)(a-Math.sqrt(a*a-4*b))/2};
         return answer;
     }
 }
