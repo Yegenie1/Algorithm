27d8f7a2eab3334bb11d653af744f77743028675
yejineeeeeeeee <124111019+Yegenie1@users.noreply.github.com>
2023-06-29 14:51:11 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,9 +1,24 @@
-import java.util.*;
 class Solution {
-    public int[] solution(int brown, int red) {
-        int a = (brown+4)/2;
-        int b = red+2*a-4;
-        int[] answer = {(int)(a+Math.sqrt(a*a-4*b))/2,(int)(a-Math.sqrt(a*a-4*b))/2};
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+		int sum = brown + yellow;
+		
+		for (int i = 3; i < sum; i++) {//yellow가 존재하기 위해서는 가로, 세로 3이상
+			int j = sum / i;//sum의 약수 구하기
+			
+			if (sum % i == 0 && j >= 3) {
+				int col = Math.max(i, j);//문제에서 가로가 더 길다고 했으므로 max
+        int row = Math.min(i, j);//세로는 min
+				int yellowArea = (col - 2) * (row - 2);
+				
+				if (yellowArea == yellow) {
+					answer[0] = col;
+					answer[1] = row;
+					return answer;
+				}
+			}
+		}
+        
         return answer;
     }
 }
