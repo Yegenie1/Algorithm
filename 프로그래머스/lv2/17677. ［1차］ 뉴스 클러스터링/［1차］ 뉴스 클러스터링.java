be242e8b8e8a4436ae23e0c0f9eec6fa1c92b106
yejineeeeeeeee <124111019+Yegenie1@users.noreply.github.com>
2023-08-06 20:57:22 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 2.14 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        List<String> list1 = new ArrayList<String>();
+		List<String> list2 = new ArrayList<String>();
+		
+		for (int i = 0; i < str1.length()-1; i++) {
+			String substring = str1.toUpperCase().substring(i, i + 2);
+		    if (substring.matches("[A-Z]{2}")) { 
+		    	list1.add(substring);
+		    }
+		}
+		for (int i = 0; i < str2.length()-1; i++) {
+			String substring = str2.toUpperCase().substring(i, i + 2);
+		    if (substring.matches("[A-Z]{2}")) { 
+		    	list2.add(substring);
+		    }
+		}
+		
+		Collections.sort(list1);
+		Collections.sort(list2);
+		List<String> union = new ArrayList<String>();
+		List<String> intersection = new ArrayList<String>();
+		for (String s : list1) {
+			 if(list2.remove(s)){
+				 intersection.add(s);
+	         }
+	         union.add(s);
+		}
+		
+		for(String s : list2){
+	          union.add(s);
+	    }
+        int j = 0;
+        if (union.size() == 0) {
+			answer = 65536;
+		}else{
+            answer = 65536 * intersection.size() / union.size();
+        }
+		
+        return answer;
+    }
+}
