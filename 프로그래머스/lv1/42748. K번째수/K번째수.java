36c2c63b195b1118155e71ce4f0c6e0788c0dbfa
yejineeeeeeeee <124111019+Yegenie1@users.noreply.github.com>
2023-08-17 19:09:02 +0900

    [level 1] Title: K번째수, Time: 0.51 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,16 +1,14 @@
-import java.util.*;
+import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        List<Integer> list = new ArrayList<>();
-		int[] answer = new int[commands.length];
-		for (int i = 0; i < answer.length; i++) {
-			for (int j = commands[i][0]-1; j < commands[i][1]; j++) {
-				list.add(array[j]);
-			}
-			Collections.sort(list);
-			answer[i] = list.get(commands[i][2]-1);
-			list.clear();
-		}
+        int[] answer = new int[commands.length];
+
+        for(int i=0; i<commands.length; i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+
         return answer;
     }
 }
