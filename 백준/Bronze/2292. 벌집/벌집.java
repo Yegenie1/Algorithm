6a5f0bdfa229ff4a4d68024e66cd620f6f84ecef
yejineeeeeeeee <124111019+Yegenie1@users.noreply.github.com>
2024-01-28 16:17:42 +0900

    [Bronze II] Title: 벌집, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int count = 1;
+        int limit = 1;
+        int i = 1;
+        while(true) {
+        	if (N <= limit) {
+				break;
+			}
+			limit += 6 * i;
+			count++;
+			i++;
+        }
+        System.out.println(count);
+        
+        br.close();
+    }
+}
