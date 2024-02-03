8df64b363feee1a4caeff1ddde1dc4eaa533714e
yejineeeeeeeee <124111019+Yegenie1@users.noreply.github.com>
2024-02-03 18:35:33 +0900

    [Silver V] Title: 단어 정렬, Time: 360 ms, Memory: 27084 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -2,21 +2,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        Set<String> set = new HashSet<String>();
-        for (int i = 0; i < n; i++) {
-        	set.add(br.readLine());
+        String[] arr = new String[n];
+        for (int i = 0; i < arr.length; i++) {
+			arr[i] = br.readLine();
 		}
-        String[] arr = new String[set.size()];
-        set.toArray(arr);
-        
         Arrays.sort(arr, (s1, s2) ->{
         	if (s1.length() == s2.length()) {
 				return s1.compareTo(s2);
@@ -25,8 +20,13 @@ public class Main {
 			}
         });
         
-        for (int i = 0; i < arr.length; i++) {
-			System.out.println(arr[i]);
+        sb.append(arr[0]).append('\n');
+        
+        for (int i = 1; i < arr.length; i++) {
+			if (!arr[i].equals(arr[i-1])) {
+				sb.append(arr[i]).append('\n');
+			}
 		}
+        System.out.println(sb);
     }
 }
