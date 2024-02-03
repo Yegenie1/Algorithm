0edd2c0e251cefb5451767aff0d2e77afb938c3a
yejineeeeeeeee <124111019+Yegenie1@users.noreply.github.com>
2024-02-03 18:32:23 +0900

    [Silver V] Title: 단어 정렬, Time: 620 ms, Memory: 21952 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,36 +17,16 @@ public class Main {
         String[] arr = new String[set.size()];
         set.toArray(arr);
         
-        for (int i = 0; i < arr.length; i++) {
-			for (int j = i+1; j < arr.length; j++) {
-				if (arr[i].length() > arr[j].length()) {
-					String temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}else if (arr[i].length() == arr[j].length()) {
-					if (dictionary(arr[i], arr[j]).equals(arr[i])) {
-						String temp = arr[i];
-						arr[i] = arr[j];
-						arr[j] = temp;
-					}
-				}
-			}
-			sb.append(arr[i]).append('\n');
-		}
-        System.out.println(sb);
-        br.close();
-    
-    }
-    public static String dictionary(String a, String b) {
-		for (int i = 0; i < a.length(); i++) {
-			if (a.charAt(i) > b.charAt(i)) {
-				return a;
-			}else if(a.charAt(i) == b.charAt(i)) {
-				continue;
+        Arrays.sort(arr, (s1, s2) ->{
+        	if (s1.length() == s2.length()) {
+				return s1.compareTo(s2);
 			}else {
-				return b;
+				return s1.length() - s2.length();
 			}
+        });
+        
+        for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
 		}
-		return b;
-	}
+    }
 }
