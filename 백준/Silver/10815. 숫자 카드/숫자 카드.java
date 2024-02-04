a736cc8f95b1e39211e8fdd3da36c185a2305ece
yejineeeeeeeee <124111019+Yegenie1@users.noreply.github.com>
2024-02-04 16:53:43 +0900

    [Silver V] Title: 숫자 카드, Time: 884 ms, Memory: 121260 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,53 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        int[] card = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-			card[i] = Integer.parseInt(st.nextToken());
-		}
-        Arrays.sort(card);
-        
-        int m = Integer.parseInt(br.readLine());
+        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        
+
+        int n, m;
+        HashMap<String, Integer> owned = new HashMap<>();
+
+        n = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			sb.append(binarySearch(card, n, num)).append(" ");
-		}
-        
-        System.out.println(sb);
-        br.close();
-    }
-    
-    public static int binarySearch(int[] cards, int N, int search) {
-        int first = 0;
-        int last = N - 1;
-        int mid = 0;
- 
-        while (first <= last) {
-            mid = (first + last) / 2;
- 
-            if (cards[mid] == search) {
-                return 1;
-            }
- 
-            if (cards[mid] < search) {
-                first = mid + 1;
+        for (int i=0; i<n; i++) {
+            owned.put(st.nextToken(), 0);
+        }
+
+        m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<m; i++) {
+            if (owned.get(st.nextToken()) != null) {
+                sb.append("1 ");
             } else {
-                last = mid - 1;
+                sb.append("0 ");
             }
         }
- 
-        return 0;
+        System.out.println(sb.toString());
     }
 }
