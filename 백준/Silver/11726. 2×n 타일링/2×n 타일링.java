5d051383b820906a54dee62ca49cce84bd0fd1c1
yejineeeeeeeee <124111019+Yegenie1@users.noreply.github.com>
2024-02-23 17:49:32 +0900

    [Silver III] Title: 2×n 타일링, Time: 76 ms, Memory: 11644 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -3,16 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+	static Integer[] dp;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[] dp = new int[1001];
+		dp = new Integer[1001];
 		dp[1] = 1;
 		dp[2] = 2;
-		for (int i = 3; i <= n; i++) {
-			dp[i] = (dp[i-1] + dp[i-2]) % 10007;
-		}
-		System.out.println(dp[n]);
+		System.out.println(tiling(n));
 		br.close();
+	}
+	public static int tiling(int n) {
+		if (dp[n] != null) {
+			return dp[n];
+		}
+		dp[n] = (tiling(n-1) + tiling(n-2)) % 10007;
+		return dp[n];
 	}
 }
