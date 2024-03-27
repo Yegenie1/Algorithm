ea4bd2ab8569b96d5d950259a1bcaef2e17e661e
yejineeeeeeeee <124111019+Yegenie1@users.noreply.github.com>
2024-03-27 19:03:34 +0900

    [Silver IV] Title: 수 찾기, Time: 1756 ms, Memory: 66184 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,30 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+	public static int[] arr;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		Set<Integer> set = new HashSet<Integer>();
+		arr = new int[N];
 		
 		for (int i = 0; i < N; i++) {
-			set.add(Integer.parseInt(st.nextToken()));
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
+		Arrays.sort(arr);
 		int M = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < M; i++) {
 			int num = Integer.parseInt(st.nextToken());
-			if (set.contains(num)) {
+			if (binarySearch(num) >= 0) {
 				System.out.println(1);
 			}else {
 				System.out.println(0);
 			}
 		}
 		br.close();
+	}
+	public static int binarySearch(int key) {
+		int low = 0;
+		int high = arr.length-1;
+		
+		while(low <= high) {
+			int mid = (low+high)/2;
+			
+			if (key < arr[mid]) {
+				high = mid - 1;
+			}else if(key > arr[mid]) {
+				low = mid + 1;
+			}else {
+				return mid;
+			}
+		}
+		return -1;
 	}
 }
